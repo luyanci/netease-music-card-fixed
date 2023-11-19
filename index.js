ab2a7db0147ce5a51d72775aec6037ee166ef4db
luyanci <68143180+luyanci@users.noreply.github.com>
2023-11-19 20:05:17 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const {
     })
     const username = account.body.profile.nickname;
     const avatarUrl = account.body.profile.avatarUrl + "?param=128y128"; // 压缩
-    console.log(`个人头像: ${avatarUrl}`);
+    console.log(`用户名： ${username}个人头像: ${avatarUrl}`);
 
     /*
       获取歌单记录
